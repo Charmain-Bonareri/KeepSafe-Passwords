56fafee13836b0ebc362bdb4b2660d830c33fb1e
Charmain <mogerecharmain@gmail.com>
2022-04-24 06:43:42 +0300

    Tested if our objects are being instantiated correctly.

--- user_test.py
+++ user_test.py
@@ -9,4 +9,14 @@ class UserTest(unittest.TestCase):
         self.new_user = User("CharmainB","charmb@gmail.com","Mogz123!") # create user object
 
 
-    
+    def test_init(self):
+        """
+            Test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.username, "CharmainB")        
+        self.assertEqual(self.new_user.email, "charmb@gmail.com")
+        self.assertEqual(self.new_user.password, "Mogz123!")
+
+
+if __name__ == '__main__':
+    unittest.main()
